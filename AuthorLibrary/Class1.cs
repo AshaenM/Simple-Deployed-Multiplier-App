4fcb6388640fce70f1fec45b432d9019cbbc9159
AshaenM <ashaenmanuel41@gmail.com>
2025-08-23 03:49:27 +1000

    Added all source code

---
+++ AuthorLibrary/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthorLibrary
+{
+    public class Author
+    {
+        public static string MainAuthor() => "Ashaen Manuel";
+    }
+}
